2e0fb2fdbdc5f034e41c8844c4350fdfb637b0f1
Jaume Riera <jaumerg95@gmail.com>
2023-02-05 12:17:42 +0100

    Adjust enemy scripts

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -8,20 +8,31 @@ public class Enemy : PoolableObject
 {
     [SerializeField]
     private EnemyScriptableObject enemySettings;
+    [SerializeField]
+    Animator animator;
 
     HealthManager healthManager;
+    Rigidbody rb;
 
+    [Header("Float Variables")]
     [SerializeField]
     private FloatVariable score;
     [SerializeField]
     private FloatVariable ResourcesToGrowRoots;
 
     protected void Start() {
+        rb = GetComponent<Rigidbody>();
         healthManager = GetComponent<HealthManager>();
         healthManager.SetUp(enemySettings.health);
     }
 
+    public void TakeDamage(float damage) {
+        healthManager.TakeDamage(damage);
+    }
+
     public void DieAction() {
+        animator.SetTrigger("Die");
+        rb.detectCollisions = false;
         score.ApplyChange(enemySettings.points);
         ResourcesToGrowRoots.ApplyChange(enemySettings.reward);
         // implement anim or something
