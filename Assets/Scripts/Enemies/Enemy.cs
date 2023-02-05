020466a9830272cabb221cf23dac043cf8fe41ba
Jaume Riera <jaumerg95@gmail.com>
2023-02-05 13:20:30 +0100

    Make enemy follow path

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -2,14 +2,25 @@ using UnityEngine;
 
 using FeTo.ObjectPool;
 using FeTo.SOArchitecture;
+using System.Linq;
+using JetBrains.Annotations;
 
 [RequireComponent(typeof(HealthManager))]
 public class Enemy : PoolableObject
 {
     [SerializeField]
-    private EnemyScriptableObject enemySettings;
-    [SerializeField]
     Animator animator;
+    [SerializeField]
+    GameEvent gameOverEvent;
+
+    [Header("Scriptables settings")]
+    [SerializeField]
+    EnemyScriptableObject enemySettings;
+    [SerializeField]
+    PathScriptableObject enemyPath;
+
+    private Vector3 target;
+    private int currentTarget;
 
     HealthManager healthManager;
     Rigidbody rb;
@@ -26,6 +37,12 @@ public class Enemy : PoolableObject
         healthManager.SetUp(enemySettings.health);
     }
 
+    private void OnEnable() {
+        currentTarget = 0;
+        target = enemyPath.path[0];
+        transform.LookAt(target);
+    }
+
     public void TakeDamage(float damage) {
         healthManager.TakeDamage(damage);
     }
@@ -36,5 +53,22 @@ public class Enemy : PoolableObject
         score.ApplyChange(enemySettings.points);
         ResourcesToGrowRoots.ApplyChange(enemySettings.reward);
         // implement anim or something
+    }
+
+    private void Update() {
+        if (Vector3.Distance(transform.position, target) <= 0) {
+            SetNextTarget();
+        }
+        transform.position = Vector3.MoveTowards(transform.position, target, enemySettings.speed * Time.deltaTime);
+    }
+
+    private void SetNextTarget() {
+        currentTarget += 1;
+        if(currentTarget >= enemyPath.path.Count) {
+            gameOverEvent.Raise();
+        } else {
+            target = enemyPath.path[currentTarget];
+            transform.LookAt(target);
+        }
     }
 }
