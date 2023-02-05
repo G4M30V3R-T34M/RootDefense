09c5867976b8b6cc8255b8130365df3f38a13758
Jaume Riera <jaumerg95@gmail.com>
2023-02-05 09:42:10 +0100

    Create scriptable for accorn

---
+++ Assets/Scripts/ScriptableObjects/AccornScriptableObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AccornScriptableObject", menuName = "Scriptables/AccornScriptableObject", order = 4)]
+
+public class AccornScriptableObject : ScriptableObject
+{
+    public float speed;
+}
