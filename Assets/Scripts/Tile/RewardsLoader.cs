f7f31980012869363f2ad1132448b894018d95f8
Neengash <pepmanas93@gmail.com>
2023-02-05 02:24:41 +0100

    Rename techtreemanager to rewards loader

--- Assets/Scripts/Tile/RewardsLoader.cs
+++ Assets/Scripts/Tile/RewardsLoader.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TechTreeManager : MonoBehaviour
+public class RewardsLoader : MonoBehaviour
 {
     [SerializeField] Map tileMap;
 
