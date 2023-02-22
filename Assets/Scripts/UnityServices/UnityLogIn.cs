3d6124ada80b9db65c4e0e730e68d5e947c6f3b2
Jaume Riera <jaumerg95@gmail.com>
2023-02-22 19:48:46 +0100

    Ignore autentication if exists an error due authentication

--- Assets/Scripts/UnityServices/UnityLogIn.cs
+++ Assets/Scripts/UnityServices/UnityLogIn.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using System.Threading.Tasks;
 using UnityEngine.Events;
 using FeTo.SOArchitecture;
+using System.Net;
+using System;
 
 public class UnityLogIn : MonoBehaviour
 {
@@ -14,18 +16,20 @@ public class UnityLogIn : MonoBehaviour
     }
 
     async Task InitializeRemoteConfigAsync() {
-        // initialize handlers for unity game services
-        await UnityServices.InitializeAsync();
+        try {
+            // initialize handlers for unity game services
+            await UnityServices.InitializeAsync();
 
-        // options can be passed in the initializer, e.g if you want to set analytics-user-id or an environment-name use the lines from below:
-        // var options = new InitializationOptions()
-        //   .SetOption("com.unity.services.core.analytics-user-id", "my-user-id-1234")
-        //   .SetOption("com.unity.services.core.environment-name", "production");
-        // await UnityServices.InitializeAsync(options);
+            // options can be passed in the initializer, e.g if you want to set analytics-user-id or an environment-name use the lines from below:
+            // var options = new InitializationOptions()
+            //   .SetOption("com.unity.services.core.analytics-user-id", "my-user-id-1234")
+            //   .SetOption("com.unity.services.core.environment-name", "production");
+            // await UnityServices.InitializeAsync(options);
 
-        // remote config requires authentication for managing environment information
-        if (!AuthenticationService.Instance.IsSignedIn) {
-            await AuthenticationService.Instance.SignInAnonymouslyAsync();
-        }
+            // remote config requires authentication for managing environment information
+            if (!AuthenticationService.Instance.IsSignedIn) {
+                await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            }
+        } catch (RequestFailedException) { }
     }
 }
